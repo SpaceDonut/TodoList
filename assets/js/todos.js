523eb3f6afe48aa76a16e3122786bf89c2ed986f
SpaceDonut <rinkusu3@gmail.com>
2020-04-29 12:30:43 +0900

    Added keypress event for enter on input , adding text to a var

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,9 +3,16 @@ $('li').click(function(){
    $(this).toggleClass("completed");
 })
 
-$("span").on('click', function (e) { 
+$("span").on('click', function (e) {
    $(this).parent().fadeOut(400, function(){
       $(this).remove();
    });
    e.stopPropagation();
 });
+
+$("input[type='text'").on("keypress", function(e){
+   if(e.which === 13){
+      //grabbing new todo text from input
+      let todoText = $(this).val();
+   }
+})
